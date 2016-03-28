adaf32b7d07f824bbfda17cd1e4b70328ed27b0d
Luis San Pablo <luis.sanpablo@utexas.edu>
2016-03-28 05:42:48 -0500

    Create color_chooser.cs

---
+++ color_chooser.cs
@@ -0,0 +1,95 @@
+using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+using GH_IO;
+using GH_IO.Serialization;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+
+/// <summary>
+/// This class will be instantiated on demand by the Script component.
+/// </summary>
+public class Script_Instance : GH_ScriptInstance
+{
+#region Utility functions
+  /// <summary>Print a String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="text">String to print.</param>
+  private void Print(string text) { /* Implementation hidden. */ }
+  /// <summary>Print a formatted String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="format">String format.</param>
+  /// <param name="args">Formatting parameters.</param>
+  private void Print(string format, params object[] args) { /* Implementation hidden. */ }
+  /// <summary>Print useful information about an object instance to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj) { /* Implementation hidden. */ }
+  /// <summary>Print the signatures of all the overloads of a specific method to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj, string method_name) { /* Implementation hidden. */ }
+#endregion
+
+#region Members
+  /// <summary>Gets the current Rhino document.</summary>
+  private readonly RhinoDoc RhinoDocument;
+  /// <summary>Gets the Grasshopper document that owns this script.</summary>
+  private readonly GH_Document GrasshopperDocument;
+  /// <summary>Gets the Grasshopper script component that owns this script.</summary>
+  private readonly IGH_Component Component;
+  /// <summary>
+  /// Gets the current iteration count. The first call to RunScript() is associated with Iteration==0.
+  /// Any subsequent call within the same solution will increment the Iteration count.
+  /// </summary>
+  private readonly int Iteration;
+#endregion
+
+  /// <summary>
+  /// This procedure contains the user code. Input parameters are provided as regular arguments,
+  /// Output parameters as ref arguments. You don't have to assign output parameters,
+  /// they will have a default value.
+  /// </summary>
+  private void RunScript(List<string> options_bear_grass, List<string> options_blue_stem, List<string> options_yucca, List<int> plant_type, List<int> selected_seasons, string death_color, List<int> plant_states, ref object selected_colors)
+  {
+    List<string> chosen_colors = new List<string>();
+
+    for (int i = 0; i < plant_type.Count; i++) {
+      if (plant_states[i] == 2) {
+        chosen_colors.Add(death_color);
+      } else {
+        switch(plant_type[i]){
+          case 0:
+            chosen_colors.Add(options_bear_grass[selected_seasons[i]]);
+            break;
+          case 1:
+            chosen_colors.Add(options_yucca[selected_seasons[i]]);
+            break;
+          default:
+            chosen_colors.Add(options_blue_stem[selected_seasons[i]]);
+            break;
+        }
+      }
+    }
+    selected_colors = chosen_colors;
+  }
+
+  // <Custom additional code> 
+
+  // </Custom additional code> 
+}
